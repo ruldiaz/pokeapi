d5c3235cba6a7a9d3ae0411a4d74fa425b549a6b
ruldiaz <rulhdiaz@gmail.com>
2024-11-24 13:14:33 -0600

    commit

--- poke-app/src/app/components/general/navbar/Navbar.tsx
+++ poke-app/src/app/components/general/navbar/Navbar.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const Navbar = () => {
   return (
     <nav className='flex bg-green-800 bg-opacity-30 p-2 m-2 rounded fixed top-0 left-0 w-full z-50'>
-      <a href="/">Home</a>
+      <Link href='/'>Home</Link>
       <div className='flex flex-1'></div>
-      <a className='mr-2' href="/pokemon">Pokemon List</a>
-      <a className='mr-2' href="/contact">Contact</a>
+      <Link href="/pokemon">Pokemon List</Link>
+      <Link href='/contact'>Contact</Link>
     </nav>
   )
 }
