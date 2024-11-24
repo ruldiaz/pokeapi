634f53dbb7974571d9903fdc894eb4955d71d319
ruldiaz <rulhdiaz@gmail.com>
2024-11-24 13:16:03 -0600

    style to navbar

--- poke-app/src/app/components/general/navbar/Navbar.tsx
+++ poke-app/src/app/components/general/navbar/Navbar.tsx
@@ -6,8 +6,8 @@ export const Navbar = () => {
     <nav className='flex bg-green-800 bg-opacity-30 p-2 m-2 rounded fixed top-0 left-0 w-full z-50'>
       <Link href='/'>Home</Link>
       <div className='flex flex-1'></div>
-      <Link href="/pokemon">Pokemon List</Link>
-      <Link href='/contact'>Contact</Link>
+      <Link className='m-2' href="/pokemon">Pokemon List</Link>
+      <Link className='m-2' href='/contact'>Contact</Link>
     </nav>
   )
 }
